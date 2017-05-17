a804569cad61032ffad352fa594a41e425b716ff
michal <michal.zurek7@gmail.com>
2017-05-17 20:37:28 +0200

    added esp stuff

--- podlewacz_esp.ino
+++ podlewacz_esp.ino
@@ -1,5 +1,3 @@
-
-// ------------- hardware setup ------------------
 #define powerOnPin 12                // ESP8266 CH_PD
 #define softRX 3                     // soft RX on Arduino
 #define softTX 2                     // soft TX on Arduino
@@ -7,27 +5,24 @@
 #define configDelay 20000            // max time in ms to wait for ESP8266 comes up
 #define serialDelay 200              // delay between two serial prints to ESP8266
 
-// ------------- mode setup ------------------
-#define lowPowerMode 0               // lowPower = 1, standard = 0s
-
-// ------------- time setup ------------------
-#define wifiSendPeriod 300000       // send data every x ms to thingspeak
-#define meanPeriod 300000            // calculate the average of the sensor values of the last x ms
-#define avAnzDef 60                  // number of values within the meanPeriod for averaging
-
-// ------------- additional libraries ------------------
 #include <Wire.h>
 #include <LCD.h>
 #include <LiquidCrystal_I2C.h>
+#include <SoftwareSerial.h>
+#include <SimpleTimer.h>
 
 #define ttp229 (0xAF>>1)
 
-// soft-serial
-#include <SoftwareSerial.h>
 SoftwareSerial wifiPort(softRX, softTX);   // RX, TX
+LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address Ox3F (Check yours)
+SimpleTimer timer;
 
-//---------------------------------
+// Soft-Serial
+String inputStringWifi = "";         // a string to hold incoming data
+String inputStringHWifi = "";
+boolean stringCompleteWifi = false;  // whether the string is complete
 
+//------ touch panel
 uint16_t data_out = 0;
 uint16_t data1, data2;
 int count = 0;
@@ -35,8 +30,8 @@ boolean count2 = false;
 int button[16];
 int oldata[16];
 
-const int stepPin = 2;
-const int dirPin = 3;
+//const int stepPin = 2;
+//const int dirPin = 3;
 
 const int pumpPin = 8;
 
@@ -44,39 +39,14 @@ const int soilInputPin1 = A1;
 const int soilInputPin2 = A2;
 const int soilInputPin3 = A3;
 
-LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address Ox3F (Check yours)
-
 boolean backlight = false;
 
 int li = 0;
-
-
-// ------------- variables WiFi and serial------------------
-// time
-long time = 0;
-float timeSent = 0;
-
-// ADC read intervall and average period
-long meanUpdateIntervall;
-long timeAverage = 0;
-
-// ESP01
-int wifiStat = 0;
-int wifiSentStat = 0;
-long wifiSendTime = 0;
-int confStat = -1;
 
 // Serial Input
 String inputString = "";         // a string to hold incoming data
 String inputStringH = "";
 boolean stringComplete = false;  // whether the string is complete
-
-// Soft-Serial
-String inputStringWifi = "";         // a string to hold incoming data
-String inputStringHWifi = "";
-boolean stringCompleteWifi = false;  // whether the string is complete
-int sendV = 0;
-int sendStat = 0;
 
 
 //---------- Setup ------------
@@ -85,9 +55,9 @@ void setup() {
   Wire.begin();
   lcd.begin(16, 2); // defines it is a 16 character two line display
   lcd.noBacklight();
-  
-  pinMode(stepPin, OUTPUT);
-  pinMode(dirPin, OUTPUT);
+
+//  pinMode(stepPin, OUTPUT);
+//  pinMode(dirPin, OUTPUT);
 
   pinMode(pumpPin, OUTPUT);
   digitalWrite(pumpPin, LOW);
@@ -100,26 +70,22 @@ void setup() {
 
   Serial.begin(115200);
 
-  // Set reset high
   pinMode(powerOnPin, OUTPUT);
   digitalWrite(powerOnPin, LOW);
-
-  // set the data rate for the SoftwareSerial port
+  delay(1000);
+  digitalWrite(powerOnPin, HIGH);
   wifiPort.begin(9600);
-  // delay(1000);
+  delay(1000);
+//  softSerialEvent();
 
-  // Set ADC update time intervall
-  meanUpdateIntervall = meanPeriod / avAnzDef;
+  timer.setInterval(15000, repeatMe);
 
-  // Set times
-  time = millis();
-  wifiSendTime = time - wifiSendPeriod;
-
-  Serial.println("Start");
+  Serial.println("Arduino after setup...");
 }
 
 void loop() {
-
+  softSerialEvent();
+  timer.run();
   getTouch();
 
   if (button[0] != 7 && button[0] != 15) { //tryb default
@@ -128,96 +94,20 @@ void loop() {
     lcd.print(button[0], 1); // 0 creates whole number, 1 two decimal
     lcd.print(" wcisniety");
     lcd.setCursor(0, 1); // start postion of temperature text on LCD
-    lcd.print(6, 1);
-    lcd.print(" TRYB WIFI");
-
-    //TODO: oryginalny projekt zawiera logike czujnika oswietlenia
-
-    // New Time
-    time = millis();
-
-    // ADC Read
-    if (time - timeAverage >= meanUpdateIntervall) {
-      timeAverage = time;
-    }
-
-    // Check Wifi UP Time
-    if (time - wifiSendTime > wifiSendPeriod) {
-      wifiStat = 1;
-      wifiSendTime = time;
-      Serial.println("-> Start ESP");
-    }
-
-    // Control ESP8266
-    if (wifiStat == 1) {
-      wifiStat = 0;
-      confStat = 0;
-      wifiSentStat = 0;
-      digitalWrite(powerOnPin, HIGH);
-      Serial.println("-> ESP On");
-    } else if (wifiStat == -1) {
-      wifiStat = 0;
-      confStat = -1;
-      wifiSentStat = -1;
-      digitalWrite(powerOnPin, LOW);
-      Serial.println("-> ESP Off");
-    }
-
-    // Check conf intervall
-    if (confStat == 0 && time - wifiSendTime > configDelay) {
-      wifiStat == 0;
-      wifiSendTime = time;
-      wifiSentStat = 0;
-
-      restartESP();
-      Serial.println("-> ESP restart");
-    }
-
-    // Send Data to ESP8266
-    if (wifiSentStat == 0 && confStat == 1) {
-
-      // Soil sensor 1
-      wifiPort.print("field1=");
-      //    wifiPort.println(temp,2); TODO!!!!!!!!!!!
-      wifiPort.println("");
-      delay(serialDelay);
-
-      // Send data
-      wifiPort.println("sendData()");
-      wifiSentStat = 1;
-
-      Serial.println("Sent Data");
-    }
-
-    // Process Soft Serial Input
-    softSerialEvent();
-
-    if (stringCompleteWifi) {
-
-      Serial.println(inputStringWifi);
-
-      if (inputStringWifi == "WS1") {
-        confStat = 1;
-      } else if (inputStringWifi == "D1" && lowPowerMode == 1) {
-        wifiStat = -1;
-        // wifiSentStat = 1;
-      }
-
-      inputStringWifi = "";
-      stringCompleteWifi = false;
-    }
+    //    lcd.print(6, 1);
+    lcd.print("WIFI + TOUCH");
   }
 
   if (button[0] == 7) { //tryb ruch silnika
 
-    digitalWrite(dirPin, HIGH); // Enables the motor to move in a particular direction
-    for (int x = 0; x < 20000; x++) {
-      digitalWrite(stepPin, HIGH);
-      delay(1500);
-      digitalWrite(stepPin, LOW);
-      delay(1500);
-    }
-    delay(1000); // One second delay
+//    digitalWrite(dirPin, HIGH); // Enables the motor to move in a particular direction
+//    for (int x = 0; x < 20000; x++) {
+//      digitalWrite(stepPin, HIGH);
+//      delay(1500);
+//      digitalWrite(stepPin, LOW);
+//      delay(1500);
+//    }
+//    delay(1000); // One second delay
 
     //    digitalWrite(dirPin, HIGH); // Enables the motor to move in a particular direction
     //    // Makes 200 pulses for making one full cycle rotation
@@ -262,17 +152,19 @@ void loop() {
   if (button[0] == 10) { //tryb wlacz pompke
     lcd.clear();
     lcd.setCursor(0, 0);
-
     digitalWrite(pumpPin, HIGH);
     lcd.print("Pump turned ON");
+    delay(2000);
+    button[0] = 0;
   }
 
   if (button[0] == 9) { //tryb wylacz pompke
     lcd.clear();
     lcd.setCursor(0, 0);
-
     digitalWrite(pumpPin, LOW);
     lcd.print("Pump turned OFF");
+    delay(2000);
+    button[0] = 0;
   }
 
   if (button[0] == 1) { //zgas zapal swiatlo
@@ -288,17 +180,20 @@ void loop() {
     }
   }
 
+  if (button[0] == 14) { //show ip for 5s
+    lcd.clear();
+    wifiPort.println("getIp()");
+    delay(serialDelay);
+  }
 
   delay(100);
-
 }
 
 // Soft Serial Input
 void softSerialEvent() {
-  
+
   wifiPort.listen();
   if (wifiPort.available()) {
-    Serial.println("ss event");
     // get the new byte:
     char inChar = (char)wifiPort.read();
 
@@ -308,6 +203,25 @@ void softSerialEvent() {
       inputStringWifi += inChar;
     }
   }
+}
+
+// a function to be executed periodically
+void repeatMe() {
+  //  Serial.println("Invoking ESP...");
+  //
+  wifiPort.print("field1=");
+  wifiPort.println(50);
+  wifiPort.println("");
+  delay(serialDelay);
+
+  wifiPort.println("sendThingspeak()");
+//  softSerialEvent();
+
+  //
+  //  wifiPort.println("sendTest()");
+  //  delay(serialDelay);
+  //  wifiPort.println("getIp()");
+  //  delay(serialDelay);
 }
 
 // Restart ESP
